3fad56c7fc807bbbb49a576c9c5fec6bef353be9
Alex Aguirre <teamaguirre@gmail.com>
2022-07-11 21:54:25 -0700

    Created the intial DB.Json file structure to hold card data intial pseudocode roadmap

---
+++ gamescripts.js
@@ -0,0 +1,37 @@
+/*These are the functions to run the card game */
+
+// Create variables to hold the values needed for the game
+
+//Intialize the Wallet function with $500 Credit
+
+// Event listener for Wager input field 
+
+    //validate the wager amount
+
+    //enable the Game Card selection buttons
+
+//Hi - Lo Game Selected Function
+
+    //Event Listeners for Hi - Lo
+
+//Red or Black game function
+    
+    //Event Listeners for Red or Black
+
+//Deal the Card randomized function
+
+    //Event Listener for Deal the next card
+
+//Deternmine win or loss and update the wallet function
+
+
+// Reset the game function after 5 seconds and deal the next card. Show the message PLAY AGAIN!
+
+
+
+
+
+
+
+
+
